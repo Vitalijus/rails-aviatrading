6f7e687016f0480f58789844265231d362f2c59f
Vitalijus <vitalij.desuk@gmail.com>
2014-09-19 03:37:35 +0200

    init

---
+++ app/views/courses/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @course, :id, :title, :teacher_name, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+School::Application.config.secret_key_base = 'b9fb0783ffde6fc30ac322802e0cc56dc26665930feac62fe62a293e87d9d7c245d6f6d3839f8c51df046bbea35791b3bb90436680ff41c1189cfa93b050ad9c'
