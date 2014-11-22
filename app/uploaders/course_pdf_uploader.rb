5b496f39a1e7704388b683ec00ce627da71f394e
Vitalijus <vitalij.desuk@gmail.com>
2014-11-22 23:48:27 +0100

    init

--- app/uploaders/course_pdf_uploader.rb
+++ app/uploaders/course_pdf_uploader.rb
@@ -1,8 +1,8 @@
 # encoding: utf-8
 
 class CoursePdfUploader < CarrierWave::Uploader::Base
-  include Cloudinary::CarrierWave
-    process :tags => ['course_pdf']
+  #include Cloudinary::CarrierWave
+  #  process :tags => ['course_pdf']
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
@@ -10,13 +10,13 @@ class CoursePdfUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   #storage :file
-  # storage :fog
+   storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  #def store_dir
-  #  "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  #end
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
@@ -41,7 +41,7 @@ class CoursePdfUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
    def extension_white_list
-     %w(pdf)
+     %w(pdf ppt)
    end
 
   # Override the filename of the uploaded files:
