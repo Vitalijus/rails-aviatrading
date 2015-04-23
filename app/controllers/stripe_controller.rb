0f56a0cb8a23214f97837956106be2d3d251cb2f
Vitalijus <vitalij.desuk@gmail.com>
2015-04-23 19:06:29 +0200

    first commit

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -2,8 +2,6 @@ class StripeController < ApplicationController
 	protect_from_forgery except: :webhook
 
 	def webhook
-		#Retrieving the event from the Stripe API guarantees its authenticity  
-    	event = Stripe::Event.retrieve(params[:id])
     	#p data_json['data']['object']['customer']
     	#if data_json[:type] == "customer.subscription.created"
     		#stripe_customer_token = data_json['data']['object']['customer']
@@ -22,17 +20,19 @@ class StripeController < ApplicationController
     	#end
     #render nothing: true  
 
+    	#Retrieving the event from the Stripe API guarantees its authenticity  
+    	event = Stripe::Event.retrieve(params[:id])
+
     	if event.type == "customer.subscription.created"
-      		stripe_customer_token = event.data.object.customer
-      		user = User.where(stripe_customer_token: stripe_customer_token).first
-      	#	@user = User.last
-    	#	@user = User.all
-    	#	@user.about = "Customer111"
-    	#	@user.save!
-      		UserMailer.new_customer_subscription(user).deliver
-      		#StripeLogger.info "Webhook received params.inspect. Did not handle this event."  
+      		customer_subscribed_to_plan
     	end  
 
     	render nothing: true
+	end
+
+	def customer_subscribed_to_plan
+		stripe_customer_token = event.data.object.customer
+  		user = User.where(stripe_customer_token: stripe_customer_token).first
+  		UserMailer.new_customer_subscription(user).deliver 
 	end
 end
