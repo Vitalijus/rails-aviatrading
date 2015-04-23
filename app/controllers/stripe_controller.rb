8345434126a5d982f5428bda0ece5eef48887e2d
Vitalijus <vitalij.desuk@gmail.com>
2015-04-23 18:51:07 +0200

    first commit

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -20,17 +20,7 @@ class StripeController < ApplicationController
     	#	@user.about = "Doesn't"
     	#	@user.save!
     	#end
-    #render nothing: true
-    	if event.type == "customer.created"
-      		stripe_customer_token = event.data.object.customer
-      		user = User.where(stripe_customer_token: stripe_customer_token).first
-      	#	@user = User.last
-    	#	@user = User.all
-    	#	@user.about = "Customer111"
-    	#	@user.save!
-      		UserMailer.new_customer_subscription(user).deliver
-      		#StripeLogger.info "Webhook received params.inspect. Did not handle this event."  
-    	end  
+    #render nothing: true 
 
     	if event.type == "customer.card.created"
       		stripe_customer_token = event.data.object.customer
@@ -54,17 +44,6 @@ class StripeController < ApplicationController
       		#StripeLogger.info "Webhook received params.inspect. Did not handle this event."  
     	end  
 
-    	if event.type == "invoice.created"
-      		stripe_customer_token = event.data.object.customer
-      		user = User.where(stripe_customer_token: stripe_customer_token).first
-      	#	@user = User.last
-    	#	@user = User.all
-    	#	@user.about = "Customer111"
-    	#	@user.save!
-      		UserMailer.new_customer_subscription(user).deliver
-      		#StripeLogger.info "Webhook received params.inspect. Did not handle this event."  
-    	end  
- 
     	render nothing: true
 	end
 end
