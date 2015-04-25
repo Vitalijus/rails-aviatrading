c50ba66ff2c19e9f1164959bf14794bd0b3576f5
Vitalijus <vitalij.desuk@gmail.com>
2015-04-26 01:46:59 +0200

    first commit

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,18 +4,18 @@ class UserMailer < ActionMailer::Base
   def customer_subscription_created(user)
     @user = user
 
-    mail(to: "info@aviatrading.com", subject: "You have been subscribed #{@user.email}")
+    mail(to: "info@aviatrading.com", subject: "You have been subscribed")
   end
 
   def charge_succeeded(user)
     @user = user
 
-    mail(to: "info@aviatrading.com", subject: "Charge succeeded #{@user.email}")
+    mail(to: "info@aviatrading.com", subject: "Charge succeeded")
   end
 
   def invoice_payment_failed(user)
     @user = user
 
-    mail(to: "info@aviatrading.com", subject: "Invoice payment failed #{@user.email}")
+    mail(to: "info@aviatrading.com", subject: "Invoice payment failed")
   end
 end
