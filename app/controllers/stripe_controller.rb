0b9d17bc7dfbe036cde0a647f463fc28787b425d
Vitalijus <vitalij.desuk@gmail.com>
2015-06-29 01:07:10 +0200

    first commit

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -26,7 +26,7 @@ class StripeController < ApplicationController
       subscription = customer.subscriptions.first.id
 
   		UserMailer.customer_subscription_created(customer.email, customer.created, 
-                subscription).deliver 
+                subscription.plan).deliver 
 	end
 
 	def charge_succeeded(event)
