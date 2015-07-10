de4fa171c1195ff8adcf886896bf4e4dbc60e746
Vitalijus <vitalij.desuk@gmail.com>
2015-07-10 02:34:07 +0200

    first commit

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,10 +1,10 @@
-#CarrierWave.configure do |config|
-#  config.fog_credentials = {
-#    :provider               => 'AWS', 
-#    :aws_access_key_id      => 'AKIAIN5PF6AX4ENVUQKQ', 
-#    :aws_secret_access_key  => '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',
-#    :region                 => 'us-west-2' 
-#  }
-#
-#  config.fog_directory  = 'aeropoly'
-#end
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS', 
+    :aws_access_key_id      => 'AKIAIN5PF6AX4ENVUQKQ', 
+    :aws_secret_access_key  => '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',
+    :region                 => 'us-west-2' 
+  }
+
+  config.fog_directory  = 'aeropoly'
+end
